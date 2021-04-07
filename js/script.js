78c0875f6d4e00da91a8caf964f59d31a6725a34
AndreaTrento95 <trentora95@gmail.com>
2021-04-07 17:02:46 +0200

    finale

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 var prezzo_base = 0.21;
 var sconto_minori = 20 / 100;
 var sconto_over = 40 / 100;
+var frase_sconto =  "Caro cliente,<br>" + "siamo lieti di informarti che grazie agli sconti sull'età l'importo finale del biglietto è di "
+var frase_no_sconto = "Caro cliente,<br>" + "il prezzo del suo biglietto corrisponde a "
+var buon_viaggio =  "Le auguriamo un buon viaggio!"
 
 // Chiedo la sua età
 var età = parseInt( prompt("Quanti anni hai?"));
@@ -15,10 +18,10 @@ var prezzo_no_sconto = prezzo_base * distanza;
 
 // Prezzo finale
 if(età < 18 ){
-    document.getElementById("output").innerHTML = "Caro cliente,<br>" + "siamo lieti di informarti che grazie agli sconti sull'età l'importo finale del biglietto è di " + (prezzo_no_sconto - (prezzo_no_sconto * sconto_minori)).toFixed(2) + " €. <br><br>" + "Le auguriamo un buon viaggio!";
+    document.getElementById("output").innerHTML = frase_sconto + (prezzo_no_sconto - (prezzo_no_sconto * sconto_minori)).toFixed(2) + " €. <br><br>" + buon_viaggio ;
 }else if(età > 65){
-    document.getElementById("output").innerHTML = "Caro cliente,<br>" + "siamo lieti di informarti che grazie agli sconti sull'età l'importo finale del biglietto è di " + (prezzo_no_sconto - (prezzo_no_sconto * sconto_over)).toFixed(2) + " €. <br><br>" + "Le auguriamo un buon viaggio!";
+    document.getElementById("output").innerHTML = frase_sconto + (prezzo_no_sconto - (prezzo_no_sconto * sconto_over)).toFixed(2) + " €. <br><br>" + buon_viaggio;
 }else{
-    document.getElementById("output").innerHTML = "Caro cliente,<br>" + "il prezzo del suo biglietto corrisponde a " + (prezzo_no_sconto).toFixed(2) + "€. <br><br>" + "Le auguriamo un buon viaggio!";
+    document.getElementById("output").innerHTML = frase_no_sconto + (prezzo_no_sconto).toFixed(2) + " €. <br><br>" + buon_viaggio;
 }
 
